2f9c7343104c27503abe2d94aa6a0effc2dcda85
amumu <345036769@qq.com>
2017-08-27 13:39:38 +0800

    增加部分文件

---
+++ Assets/Scripts/NsConfigLib/Editor/ConfigAssetImporter.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+using System.Reflection;
+
+namespace NsLib.Config
+{
+    public class ConfigAssetImporter : AssetPostprocessor {
+        
+        public static void OnPostprocessAllAssets(string[] importedAsset, string[] deletedAssets,
+            string[] movedAssets, string[] movedFromAssetPaths) {
+            if (importedAsset != null && importedAsset.Length > 0) {
+                for (int i = 0; i < importedAsset.Length; ++i) {
+                    string assetFileName = importedAsset [i];
+                    string ext = Path.GetExtension (assetFileName);
+                    if (string.Compare (ext, ".txt")) {
+                        ProcessConfigConvert (assetFileName);
+                    }
+                }
+            }
+        }
+
+        private static void InitConfigConverts()
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            System.Type[] types = asm.GetTypes ();
+            if (types == null || types.Length <= 0)
+                return;
+            for (int i = 0; i < types.Length; ++i) {
+                System.Type t = types [i];
+
+            }
+        }
+
+
+        private static void ProcessConfigConvert(string fileName)
+        {
+            string configName = Path.GetFileNameWithoutExtension (fileName);
+
+        }
+
+    }
+
+   
+
+}
