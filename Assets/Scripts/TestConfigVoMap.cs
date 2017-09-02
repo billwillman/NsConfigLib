4b0f9c3dad1bb1d5c996ea26da290ca1f18cd004
billwillman <billwillman@qq.com>
2017-09-02 14:20:55 +0800

    [增加]测试MAP

---
+++ Assets/Scripts/TestConfigVoMap.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using NsLib.Config;
+
+public class TestConfigVoMap : MonoBehaviour {
+
+    private void OnGUI() {
+
+        if (GUI.Button(new Rect(100, 100, 150, 50), "加载全部")) {
+            TextAsset asset = Resources.Load<TextAsset>("TaskStepCfg_Binary");
+            if (asset != null) {
+                ConfigVoMapMap<string, string, TaskStepVO> maps = new ConfigVoMapMap<string, string, TaskStepVO>();
+                maps.LoadFromTextAsset(asset, true);
+            }
+        }
+
+        if (GUI.Button(new Rect(250, 100, 150, 50), "加载索引")) {
+            TextAsset asset = Resources.Load<TextAsset>("TaskStepCfg_Binary");
+            if (asset != null) {
+                ConfigVoMapMap<string, string, TaskStepVO> maps = new ConfigVoMapMap<string, string, TaskStepVO>();
+                maps.LoadFromTextAsset(asset, false);
+            }
+        }
+    }
+}
