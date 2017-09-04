3e330f64090627776203b692b36a9ab26e03ae76
billwillman <billwillman@qq.com>
2017-09-04 14:09:43 +0800

    [增加]NewtonJson的Wrap库

---
+++ Assets/Scripts/NsConfigLib/JsonWrap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace NsLib.Config
+{
+    public static class JsonMapper
+    {
+        public static T ToObject<T>(string json)
+        {
+            if (string.IsNullOrEmpty (json))
+                return default(T);
+            T ret;
+            try
+            {
+                ret = JsonConvert.DeserializeObject<T> (json);
+            } catch (Exception e) {
+#if DEBUG
+                Debug.LogError(e.ToString());
+#endif
+                ret = default(T);
+            }
+
+            return ret;
+        }
+
+    }
+}
